c60e640a02542d69607c7ca1d6561ea7e9626be4
xiaoli <li.xiao1@geely.com>
2024-09-25 15:19:24 +0800

    init

--- src/api/order/index.js
+++ src/api/order/index.js
@@ -1,28 +1,5 @@
 import request from '@/utils/request'
 
-export function statisticUp(data) {
-  return request({
-    url: '/system/orderStatistics/statisticUp',
-    method: 'post',
-    data: data
-  })
-}
-
-export function statisticLow(data) {
-  return request({
-    url: '/system/orderStatistics/statisticLow',
-    method: 'post',
-    data: data
-  })
-}
-
-export function pageMerchantList(query) {
-  return request({
-      url: '/system/orderManage/pageMerchantList',
-      method: 'get',
-      params: query
-  })
-}
 
 export function pageUserList(query) {
   return request({
